a6061f3a7d12543f6d724338c3f21e267ceccd6c
Dennis <domoreburpees@icloud.com>
2023-12-15 18:23:37 -0700

    Update twin.py

--- algorithms/twin.py
+++ algorithms/twin.py
@@ -36,5 +36,5 @@ def find_twin_primes_between_squares(input_pair):
 
 # Test the function with input pair (p, p + 2)
 start_time = time.time()
-print(find_twin_primes_between_squares((1493, 1495))
+print(find_twin_primes_between_squares((1505, 1507))
 print('Time: ', (time.time() - start_time) / 60, ' minutes')
