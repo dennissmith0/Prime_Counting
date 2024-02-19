1ac22acf7aa42a93ce18699912d4ec81fb195235
George Patterson <patterson.george@gmail.com>
2024-02-19 18:45:24 -0500

    fix: cleans up multi-threaded

--- algorithms/crabby_primes/src/main.rs
+++ algorithms/crabby_primes/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::BTreeSet;
 use std::sync::Arc;
 use std::thread;
 
@@ -21,38 +21,45 @@ fn main() {
 }
 
 fn create_and_count(n: i32) -> u64 {
-    let deltas: [i8; 2] = [-1, 1];
-    let mut count = 0;
+    let deltas: [i32; 2] = [-1, 1];
     let mut poss = vec![];
-    for val in (6..n + 1).step_by(6) {
+    for (count, val) in (6..n + 1).step_by(6).enumerate() {
         for &delta in &deltas {
-            poss.push((count, val + i32::from(delta)));
-            count += 1;
+            poss.push((count, val + delta));
         }
     }
     let poss = Arc::new(poss);
 
-    let mut mult: HashSet<i32> = HashSet::new();
+    let mut mult: BTreeSet<i32> = BTreeSet::new();
 
-    let threads = num_cpus::get(); // Get the number of CPUs
+    let threads = num_cpus::get() - 1; // Get the number of CPUs
     let mut handles = vec![];
+
+    let chunk_size = poss.len() / threads;
 
     for i in 0..threads {
         let poss = Arc::clone(&poss);
         let handle = thread::spawn(move || {
-            let slice = (poss.len() * i / threads)..(poss.len() * (i + 1) / threads);
-            let local_poss = &poss[slice];
-            let mut local_mult = HashSet::new();
-            for &(idx, i) in local_poss {
-                for &(_, j) in &poss[idx..] {
-                    if let Some(product) = i.checked_mul(j) {
+            let start = chunk_size * i;
+            let end = if i == threads - 1 {
+                poss.len()
+            } else {
+                start + chunk_size
+            };
+
+            poss[start..end]
+                .iter()
+                .flat_map(|&(idx, i)| {
+                    poss[idx..].iter().map_while(move |(_, j)| {
+                        let product = i * j;
                         if product <= n {
-                            local_mult.insert(product);
+                            Some(product)
+                        } else {
+                            None
                         }
-                    }
-                }
-            }
-            local_mult
+                    })
+                })
+                .collect::<BTreeSet<_>>()
         });
         handles.push(handle);
     }
