69807ebf6b51a1accc0e3a97a3c47685d81beee6
dennissmith0 <domoreburpees@icloud.com>
2024-07-31 16:05:13 -0600

    chore: daily measure

--- algorithms/twin.py
+++ algorithms/twin.py
@@ -36,5 +36,5 @@ def find_twin_primes_between_squares(input_pair):
 
 # Test the function with input pair (p, p + 2)
 start_time = time.time()
-print(find_twin_primes_between_squares((4615, 4617)))
+print(find_twin_primes_between_squares((4651, 4653)))
 print('Time: ', (time.time() - start_time) / 60, ' minutes')
